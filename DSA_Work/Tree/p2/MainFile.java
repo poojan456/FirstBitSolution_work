a36e8af9cafc747d6d641c632271afb154ef4696
Pooja Nagapurkar <147425607+poojan456@users.noreply.github.com>
2025-10-04 21:57:23 +0530

    Add files via upload

---
+++ DSA_Work/Tree/p2/MainFile.java
@@ -0,0 +1,32 @@
+package p2;
+import pack.Node;
+
+
+public class MainFile {
+
+	public static void main(String[] args) {
+		Node n1=new Node();
+		
+		n1.insert(8);
+		n1.insert(7);
+		n1.insert(12);
+		n1.insert(15);
+		n1.insert(2);
+		n1.insert(5);
+		n1.inorder();
+		n1.preorder();
+		
+		n1.postorder();
+		
+		
+		
+		
+		
+		
+
+	}
+	
+	
+	
+
+}
